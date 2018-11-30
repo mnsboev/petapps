3e3a5e361f4582bbc9c52be48074565d34845afb
mikhail <mikhail_sboev@epam.com>
2018-11-30 18:19:38 +0300

    first commit

---
+++ src/test/kotlin/com/petapps/chat/IntegrationTest.kt
@@ -0,0 +1,24 @@
+package com.petapps.chat
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTest(@Autowired val restTemplate: TestRestTemplate) {
+
+    @Test
+    fun `Assert chat page title, content and status code`() {
+        val entity = restTemplate.getForEntity<String>("/")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>Hello</h1>")
+    }
+
+}
