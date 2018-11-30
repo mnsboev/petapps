d115ef3432b8ef20212193122f2da5d1a732dc1f
mikhail <mikhail_sboev@epam.com>
2018-11-30 18:46:59 +0300

    example of junit 5

--- src/test/kotlin/com/petapps/chat/IntegrationTest.kt
+++ src/test/kotlin/com/petapps/chat/IntegrationTest.kt
@@ -1,6 +1,8 @@
 package com.petapps.chat
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,11 +16,25 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTest(@Autowired val restTemplate: TestRestTemplate) {
 
+    @BeforeAll
+    fun setup() = println(">> setup")
+
     @Test
     fun `Assert chat page title, content and status code`() {
+        println(">> Assert blog page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(entity.body).contains("<h1>Hello</h1>")
+    }
+
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
     }
 
 }
